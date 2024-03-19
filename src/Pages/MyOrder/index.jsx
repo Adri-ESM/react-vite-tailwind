3d16116c3e4af4cc59253e0e823fa61a52fac1d4
Adri-ESM <anggivmorales@gmail.com>
2024-03-19 15:45:43 -0300

    working on My Order Components, add button continue shopping and hidden buttons when there are not any items

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,4 +1,5 @@
 import Layout from "../../Components/Layout";
+import { Link, useNavigate } from "react-router-dom";
 import OrderCard from "../../Components/OrderCard";
 import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
@@ -6,9 +7,16 @@ import Back from "../../Components/Back";
 
 function MyOrder() {
   const context = useContext(ShoppingCartContext);
+  const cartHasItems = context.order?.length > 0 && context.order[0]?.Products;
+  const cartHasItems2 = context.order?.length > 0 && context.order[0]?.Products;
+
   // const latestOrder = context.order?.slice(-1)[0];
 
- 
+//  handleChange = () => {
+//     this.setState({ show: !this.state.show });
+//  }
+
+
 
   return (
     <Layout>
@@ -34,18 +42,28 @@ function MyOrder() {
             <h2 className="text-1xl">You don&apos;t have any orders yet</h2>
           </div>
         )}
-        <div className="flex justify-between mt-10">
+        {/* <div className="flex justify-between mt-10">
           <h2 className="text-xl font-semibold">Total to Pay:</h2>
           <h2 className="text-xl font-semibold">
             ${context.order?.length > 0 && context.order[0]?.totalPrice}
           </h2>
-          </div>
+          </div> */}
       </div>
-      <button className="px-3 py-1 text-sm w-40 mt-10 font-semibold text-white bg-gray-800 rounded"
-          // onClick={handlePurchase}
-        >
-          Go To Payment
-        </button>
+      <div className="flex justify-center mt-10">
+        {cartHasItems2 && (
+          <Link to="/">
+          <button className="px-3 py-1 text-sm w-40 font-semibold text-white bg-gray-800 rounded mr-2">
+            Continue Shopping
+          </button>
+          </Link>
+        )}
+        
+        {cartHasItems && (
+          <button className="px-3 py-1 text-sm w-40 font-semibold text-white bg-gray-800 rounded ml-2">
+            Go To Payment
+          </button>
+        )}
+      </div>
     </Layout>
   );
 }
