3a78c6f3b293ea32691c08a8e6d973d758b8f733
Adri-ESM <anggivmorales@gmail.com>
2024-05-30 10:39:42 -0400

    eraser the product in order automatically when this is eraser in cart

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -10,28 +10,19 @@ function MyOrder() {
   const context = useContext(ShoppingCartContext);
   const cartHasItems = context.order?.length > 0 && context.order[0]?.Products;
   const cartHasItems2 = context.order?.length > 0 && context.order[0]?.Products;
-  //const currentPath = window.location.pathname;
-  //const index = currentPath.substring(currentPath.lastIndexOf("/") + 1);
- 
 
-  // const latestOrder = context.order?.slice(-1)[0];
-
-//  handleChange = () => {
-//     this.setState({ show: !this.state.show });
-//  }
-
-const totalToPay = context.order && Array.isArray(context.order) ? 
-context.order.reduce((total, order) => {
-  if (order.Products && Array.isArray(order.Products)) {
-    const orderTotal = order.Products.reduce((subtotal, product) => {
-      const productPrice = product.price || 0;
-      return subtotal + (productPrice * product.quantity);
-    }, 0);
-    return total + orderTotal;
-  } else {
-    return total;
-  }
-}, 0) : 0;
+  const totalToPay = context.order && Array.isArray(context.order) ? 
+  context.order.reduce((total, order) => {
+    if (order.Products && Array.isArray(order.Products)) {
+      const orderTotal = order.Products.reduce((subtotal, product) => {
+        const productPrice = product.price || 0;
+        return subtotal + (productPrice * product.quantity);
+      }, 0);
+      return total + orderTotal;
+    } else {
+      return total;
+    }
+  }, 0).toFixed(2) : "0.00"; 
 
 
 
@@ -40,9 +31,9 @@ context.order.reduce((total, order) => {
       <div>
         <Back />
       </div>
-      <h1 className="mt-40">New Order</h1>
+      <h1 className="mt-20">New Order</h1>
 
-      <div className="mt-20 flex flex-col w-80">
+      <div className="mt-10 flex flex-col w-80">
         {context.order?.length > 0 && context.order[0]?.Products ? (
           context.order[0].Products.map((product) => (
             <OrderCard
@@ -59,12 +50,6 @@ context.order.reduce((total, order) => {
             <h2 className="text-1xl">You don&apos;t have any orders yet</h2>
           </div>
         )}
-        {/* <div className="flex justify-between mt-10">
-          <h2 className="text-xl font-semibold">Total to Pay:</h2>
-          <h2 className="text-xl font-semibold">
-            ${context.order?.length > 0 && context.order[0]?.totalPrice}
-          </h2>
-          </div> */}
       </div>
       <div className="flex justify-center mt-10">
         {cartHasItems2 && (
@@ -82,8 +67,8 @@ context.order.reduce((total, order) => {
         )}
       </div>
 
-      <div className="flex justify-between mt-10">
-      <h2 className="text-xl font-semibold">Total to Pay:</h2>
+      <div className="flex justify-between mt-10 px-3 py-1 text-sm w-60 font-semibold text-white bg-gray-800 hover:bg-lime-800 rounded ml-2 hover:scale-105 hover:shadow-lg transition-transform duration-300">
+      <h2 className="text-xl font-semibold mr-2">Total to Pay:</h2>
       <h2 className="text-xl font-semibold">${totalToPay}</h2>
     </div>
 
