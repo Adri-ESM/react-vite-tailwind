5690c763a99b5604e929b441bbd744926b9defec
Adri-ESM <anggivmorales@gmail.com>
2024-08-06 11:36:26 -0400

    working on the Page My Account with SIgnOut button and SignIn Page

--- src/Firebase/index.jsx
+++ src/Firebase/index.jsx
@@ -1,35 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, signInWithEmailAndPassword, signOut } from "firebase/auth";
-//import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-//import { setLogLevel } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJYyfQVVEqC-UDPv7HKtBlvmhjGUOXGNE",
-    authDomain: "adri-s-shop.firebaseapp.com",
-    projectId: "adri-s-shop",
-    storageBucket: "adri-s-shop.appspot.com",
-    messagingSenderId: "42724334430",
-    appId: "1:42724334430:web:6b59a8a56606bf4aa10dcf",
-    measurementId: "G-53N90DB6G6"
+  apiKey: "AIzaSyCJYyfQVVEqC-UDPv7HKtBlvmhjGUOXGNE",
+  authDomain: "adri-s-shop.firebaseapp.com",
+  projectId: "adri-s-shop",
+  storageBucket: "adri-s-shop.appspot.com",
+  messagingSenderId: "42724334430",
+  appId: "1:42724334430:web:6b59a8a56606bf4aa10dcf",
+  measurementId: "G-53N90DB6G6"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-//const analytics = getAnalytics(app);
 const db = getFirestore(app); // Initialize Firestore
 
-//setLogLevel("debug");
-
 // Initialize Google Analytics only if window is defined
-// let analytics;
-// if (typeof window !== "undefined") {
-//   analytics = getAnalytics(app);
-// }
+let analytics;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
 
-export { auth, googleProvider, createUserWithEmailAndPassword, signInWithPopup, signInWithEmailAndPassword, signOut, db };
-
-
-
+export { auth, googleProvider, createUserWithEmailAndPassword, signInWithPopup, signInWithEmailAndPassword, signOut, analytics, db };
